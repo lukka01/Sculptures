427ab5d59dd7ddb479cb8ac29aca3d6fd618adf1
My Computer <khurtsidzeluka0@gmail.com>
2026-04-21 11:21:47 +0400

    Admin Panel Modified , Product errors fixed

--- MyPage/src/admin_views/utils.py
+++ MyPage/src/admin_views/utils.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 from os import path
 
-def generate_filename(object, file):
-    name, ext = path.splitext(file.filename)
-    return f"{uuid4()}{ext}"
+def generate_filename(object,  file):
+    name, extension = path.splitext(file.filename)
+    return f"{uuid4()}{extension}"
